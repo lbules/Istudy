796e5868da7bff5dbd8bfd1a5a7a6001655235cb
ridworld <48148324+ridworld@users.noreply.github.com>
2021-02-22 20:30:49 +0800

    1.增加公共模块server

---
+++ server/src/main/java/com/course/server/service/TestService.java
@@ -0,0 +1,24 @@
+package com.course.server.service;
+
+import com.course.system.domain.Test;
+import com.course.system.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Auther: luoss
+ * @Date: 2021/2/20 18:32
+ * @Description:
+ */
+
+@Service
+public class TestService {
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list() {
+        return testMapper.list();
+    }
+}
