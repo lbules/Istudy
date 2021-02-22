50bc4b7cc1fc8daed89c618b073fb47fc20de615
ridworld <48148324+ridworld@users.noreply.github.com>
2021-02-22 21:18:44 +0800

    1.通用配置移动到server（server配置文件要放在resources/config/application.properties,不能和上层路径一样放在resources根目录下）

--- system/src/main/java/com/course/system/config/SystemApplication.java
+++ system/src/main/java/com/course/system/config/SystemApplication.java
@@ -1,4 +1,4 @@
-package com.course.system;
+package com.course.system.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableEurekaClient //将服务注册到注册中心
