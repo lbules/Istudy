7d8d9c3ae60c008e010ff75a49c2e763dd6b2d4d
ridworld <48148324+ridworld@users.noreply.github.com>
2021-02-28 12:02:44 +0800

    1.新增business模块和其初始启动代码

---
+++ admin/src/router.js
@@ -0,0 +1,27 @@
+import Vue from "vue"
+import Router from "vue-router"
+import Login from "./views/login.vue"
+import Admin from "./views/admin.vue"
+import Welcome from "./views/admin/welcome.vue"
+
+Vue.use(Router);
+
+export default new Router({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [{
+        path: "*",
+        redirect: "/login",
+    },{
+        path:'/login',
+        component: Login
+    },{
+        path: '/admin',
+        component: Admin,
+        children:[{
+            // 子路由不需要加/
+            path:'welcome',
+            component: Welcome
+        }]
+    }]
+})
---
+++ admin/src/views/admin/welcome.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>welcome to Control</h1>
+    </div>
+</template>
