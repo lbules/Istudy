2050dcbb26d1747f58be934c51da6f7ff3c48493
ridworld <48148324+ridworld@users.noreply.github.com>
2021-04-03 22:46:23 +0800

    1.完成文件上传组件的开发

---
+++ admin/src/components/file.vue
@@ -0,0 +1,79 @@
+<!--文件上传公共组件-->
+
+<template>
+    <div>
+        <button type="button" v-on:click="selectFile()" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-upload"></i>
+            {{text}}
+        </button>
+        <input class="hidden" type="file" ref="file" v-on:change="uploadFile()" v-bind:id="inputId+'-input'">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'file',
+        props: {
+            text: {
+                default: "上传文件"
+            },
+            inputId: {
+                default: "file-upload"
+            },
+            afterUpload: {
+                type: Function,
+                default:null
+            },
+            suffixs: {
+                default: []
+            },
+
+        },
+        data: function () {
+            return {
+
+            }
+        },
+
+        methods: {
+            uploadFile() {
+                let _this = this;
+                let formData = new window.FormData();
+                let file = _this.$refs.file.files[0];
+
+                // 判断文件格式
+                let suffixs =_this.suffixs;
+                let fileName = file.name;
+                let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
+                let validateSuffix = false;
+                for (let i = 0; i < suffixs.length; i++) {
+                    if (suffixs[i].toLowerCase() === suffix) {
+                        validateSuffix = true;
+                        break;
+                    }
+                }
+                if (!validateSuffix) {
+                    Toast.warning("文件格式不正确！只支持上传：" + suffixs.join(","));
+                    $("#" + _this.inputId + "-input").val("");
+                    return;
+                }
+
+
+                formData.append('file',file);
+                Loading.show();
+                _this.$ajax.post(process.env.VUE_APP_SERVER+'/file/admin/upload',formData).then((response)=>{
+                    Loading.hide();
+                    let resp = response.data;
+                    let image = resp.content; //取出照片
+
+                    _this.afterUpload(resp);
+
+                });
+            },
+
+            selectFile() {
+                $("#file-upload-input").trigger("click");
+            },
+        }
+    }
+</script>
--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -93,18 +93,16 @@
                             <div class="form-group">
                                 <label class="col-sm-2 control-label">头像</label>
                                 <div class="col-sm-10">
-                                    <button type="button" v-on:click="selectImage()" class="btn btn-white btn-default btn-round">
-                                        <i class="ace-icon fa fa-upload"></i>
-                                        上传头像
-                                    </button>
-                                    <input class="hidden" type="file" ref="file" v-on:change="uploadImage()" id="file-upload-input">
+                                    <file
+                                            v-bind:suffixs="['jpg', 'jpeg', 'png']"
+                                            v-bind:id="'image-upload'"
+                                            v-bind:text="'上传头像'"
+                                            v-bind:after-upload="afterUpload"></file>
                                     <div v-show="teacher.image" class="row">
                                         <div class="col-md-4">
                                             <img v-bind:src="teacher.image" class="img-responsive">
                                         </div>
-
                                     </div>
-
                                 </div>
                             </div>
                             <div class="form-group">
@@ -142,9 +140,10 @@
 
 <script>
     import Pagination from "../../components/pagination.vue"
+    import File from "../../components/file";
 
     export default {
-        components: {Pagination},
+        components: {File, Pagination},
         name: 'teacher',
         data: function () {
             return {
@@ -238,43 +237,12 @@
                 });
             },
 
-            uploadImage() {
+            //回调函数
+            afterUpload(resp) {
                 let _this = this;
-                let formData = new window.FormData();
-                let file = _this.$refs.file.files[0];
-
-                // 判断文件格式
-                let suffixs =["jpg","jepg","png"];
-                let fileName = file.name;
-                let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
-                let validateSuffix = false;
-                for (let i = 0; i < suffixs.length; i++) {
-                    if (suffixs[i].toLowerCase() === suffix) {
-                        validateSuffix = true;
-                        break;
-                    }
-                }
-                if (!validateSuffix) {
-                    Toast.warning("文件格式不正确！只支持上传：" + suffixs.join(","));
-                    $("#" + _this.inputId + "-input").val("");
-                    return;
-                }
-
-
-                formData.append('file',file);
-                Loading.show();
-                _this.$ajax.post(process.env.VUE_APP_SERVER+'/file/admin/upload',formData).then((response)=>{
-                    Loading.hide();
-                    let resp = response.data;
-                    let image = resp.content; //取出照片
-                    console.log(image);
-                    _this.teacher.image = image;
-                });
-            },
-
-            selectImage() {
-                $("#file-upload-input").trigger("click");
-            },
+                let image = resp.content;
+                _this.teacher.image = image;
+            }
         }
     }
 </script>
